5502a59aa9eca9bda4d1abfcc78fab7af23c1f7c
Saad33-hash <saadtariq867@gmail.com>
2026-01-14 03:00:54 +0500

    Fix case sensitivity import error

--- server/Routes/contactRoutes.js
+++ server/Routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { contactUsForm} = require("../Controllers/ContactUs")
+const { contactUsForm } = require("../Controllers/contactUs")
 
 router.post("/contactUsForm", contactUsForm)
 
