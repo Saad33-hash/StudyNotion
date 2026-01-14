c0ecfd76cc7eadcf05fb40ef8238ef3c7c9a1fed
Saad33-hash <saadtariq867@gmail.com>
2026-01-14 10:32:12 +0500

    resolving connection timeout error

--- server/Utils/mailSent.js
+++ server/Utils/mailSent.js
@@ -4,9 +4,7 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-
-      secure: false, // true for 465, false for other ports
+      service: "gmail",
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
